d4f29833a36e41b3612c86516a06df7fa36ea0f8
MatheusFukuyama <mama.fukuyama@gmail.com>
2023-03-07 11:08:15 -0300

    conexão com firebase

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { getFirestore, collection, getDocs, doc, addDoc } from 'firebase/firestore/lite';
+import db from './src/connection/firebaseConfig'
 
 export default function App() {
+
+  const handleClick = () => {
+    const sensorRef = doc(db, 'sensores', 'sensor1')
+
+    addDoc(sensorRef, {
+      nome: "sensor de umidade"
+    })
+    .then(res => console.log("deu certo"))
+  
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
