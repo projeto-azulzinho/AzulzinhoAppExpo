e212b82ad599de414ca7c45f92af12ea19922654
costaemily <costa.emily@outlook.com>
2023-03-21 23:30:53 -0300

    fix: CRUD ConjuntoController.js

--- src/controllers/ConjuntoController.js
+++ src/controllers/ConjuntoController.js
@@ -1,42 +1,47 @@
 import {
-    collection,
-    getDocs,
-    addDoc,
-    updateDoc,
-    doc,
-    deleteDoc,
-  } from "firebase/firestore/lite";
-  import db from "../connection/firebaseConfig";
-  
-  const conjuntosCol = collection(db, "Conjuntos");
-  
-  export async function criarConjunto(conjunto) {
-    return await addDoc(conjuntosCol, {
-      nomeConjunto: conjunto.nomeConjunto,
-    });
-  }
-  
-  export async function atualizarConjunto(id, conjunto) {
-    const idConjunto = doc(db, "Conjuntos", `${id}`);
-    await updateDoc(idConjunto, {
-      nomeConjunto: conjunto.nomeConjunto,
-    });
-  }
-  
-  export async function deletarConjunto(id) {
-    await deleteDoc(doc(db, "Conjuntos", id));
-  }
-  
-  export async function listarConjuntos() {
-    const resp = [];
-    const querySnapshot = await getDocs(conjuntosCol);
-    querySnapshot.docs.forEach((doc) => {
-      let objecto = {
-        id: doc.id,
-        ...doc._document.data.value.mapValue.fields,
-      };
-      resp.push(objecto);
-    });
-    return resp;
-  }
-  
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+  getDoc,
+} from "firebase/firestore/lite";
+import db from "../connection/firebaseConfig";
+
+const conjuntosCol = collection(db, "Conjuntos");
+
+export async function criarConjunto(conjunto) {
+  return await addDoc(conjuntosCol, {
+    nomeConjunto: conjunto.nomeConjunto,
+  });
+}
+
+export async function consultarConjunto(id) {
+  const idConjuntoRef = doc(db, "Conjuntos", `${id}`);
+  return (await getDoc(idConjuntoRef)).data();
+}
+
+export async function atualizarConjunto(id, conjunto) {
+  const idConjuntoRef = doc(db, "Conjuntos", `${id}`);
+  await updateDoc(idConjuntoRef, {
+    nomeConjunto: conjunto.nomeConjunto,
+  });
+}
+
+export async function deletarConjunto(id) {
+  await deleteDoc(doc(db, "Conjuntos", id));
+}
+
+export async function listarConjuntos() {
+  const resp = [];
+  const querySnapshot = await getDocs(conjuntosCol);
+  querySnapshot.docs.forEach((doc) => {
+    let objecto = {
+      id: doc.id,
+      ...doc._document.data.value.mapValue.fields,
+    };
+    resp.push(objecto);
+  });
+  return resp;
+}
