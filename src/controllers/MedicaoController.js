6a0842891ee7d5c6b331fdcf0697b997d4bd0869
costaemily <emily.costa@gorilainvest.com.br>
2023-03-21 23:04:16 -0300

    fix: CRUD MedicaoController.js

--- src/controllers/MedicaoController.js
+++ src/controllers/MedicaoController.js
@@ -5,32 +5,43 @@ import {
   updateDoc,
   doc,
   deleteDoc,
+  getDoc,
 } from "firebase/firestore/lite";
 import db from "../connection/firebaseConfig";
 
-const sensorsCol = collection(db, "Medicoes");
+const medicoesCol = collection(db, "Medicoes");
 
 export async function criarMedicao(medicao) {
+  const idSensorRef = doc(db, "Sensores", `${medicao.idSensor}`);
   return await addDoc(medicoesCol, {
-    nomeMedicao: medicao.nomeMedicao,
-  });
-}
-// aquiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
-
-export async function atualizarSensor(id, sensor) {
-  const idSensor = doc(db, "Sensores", `${id}`);
-  await updateDoc(idSensor, {
-    nomeSensor: sensor.nomeSensor,
+    dataMedicao: medicao.dataMedicao,
+    valor: medicao.valor,
+    idSensor: idSensorRef,
   });
 }
 
-export async function deletarSensor(id) {
-  await deleteDoc(doc(db, "Sensores", id));
+export async function consultarMedicao(id) {
+  const idMedicaoRef = doc(db, "Medicoes", `${id}`);
+  return (await getDoc(idMedicaoRef)).data();
 }
 
-export async function listarSensores() {
+export async function atualizarMedicao(id, medicao) {
+  const idSensorRef = doc(db, "Sensores", `${medicao.idSensor}`);
+  const idMedicao = doc(db, "Medicao", `${id}`);
+  await updateDoc(idMedicao, {
+    dataMedicao: medicao.dataMedicao,
+    valor: medicao.valor,
+    idSensor: idSensorRef,
+  });
+}
+
+export async function deletarMedicao(id) {
+  await deleteDoc(doc(db, "Medicoes", id));
+}
+
+export async function listarMedicoes() {
   const resp = [];
-  const querySnapshot = await getDocs(sensorsCol);
+  const querySnapshot = await getDocs(medicoesCol);
   querySnapshot.docs.forEach((doc) => {
     let objecto = {
       id: doc.id,
