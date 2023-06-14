804bcebccd34a909ce7e7b9740ce21cd831259cc
Marina Giacchero <80406895+MarinaGiacchero@users.noreply.github.com>
2023-06-14 14:19:16 -0300

    Rastro e simulação do balão.
    Foi criado na tela localização o rastro e simulação de coordenadas do balão.

--- src/views/pages/localizacao/index.js
+++ src/views/pages/localizacao/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState} from "react";
-import { View, ScrollView, StatusBar, Text, StyleSheet} from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import { View, ScrollView, StatusBar, Text, StyleSheet, Animated } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import style from './style';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -11,70 +11,91 @@ import { ItemInformativo } from "../../components/itemInformativo";
 import { toggleMenu } from "../../../redux/menu";
 import { listarSensores, deletarSensor } from "../../../controllers/SensorController";
 import { deletarSensorDeConjunto } from "../../../controllers/SensorConjuntoController";
-import MapView from 'react-native-maps';
+import MapView, { Marker, Polyline } from 'react-native-maps';
 
-export default function Conjunto_sensores({navigation}){
+export default function Conjunto_sensores({navigation}) {
+  const { menuAtivo } = useSelector((state) => state.menuAtivo);
+  const dispatch = useDispatch();
+  const [latitude, setLatitude] = useState(-21.249025429569166);	
+  const [longitude, setLongitude] = useState(-50.31402426874828);
+  const [markerCoords, setMarkerCoords] = useState([]);
+  const markerAnimation = useRef(new Animated.Value(0)).current;
 
-    const {menuAtivo} = useSelector((state) => state.menuAtivo)
-    const dispatch = useDispatch()
-    const [latitude, setLatitude] = useState(-21.249025429569166);	
-    const [longitude, setLongitude] = useState(-50.31402426874828);
-  
-    function handleRegionChanged(region) {
-        setLatitude(region.latitude);
-        setLongitude(region.longitude);
-      }
-    
-    const abrirFecharMenu = () => {
-        dispatch(toggleMenu())
-    }
+  function handleRegionChanged(region) {
+    setLatitude(region.latitude);
+    setLongitude(region.longitude);
+    setMarkerCoords((prevCoords) => [...prevCoords, region]);
+  }
 
-    useEffect(() =>{
+  const abrirFecharMenu = () => {
+    dispatch(toggleMenu());
+  }
 
-    }, [])
-    
-    return(
-        <View style={style.viewSize}>
+  useEffect(() => {
 
-            <LinearGradient style = {style.container}
-                colors={['#063E56','#FFFFFF']} > 
-                
-                <StatusBar   
-                    backgroundColor = "#063E56"
-                    barStyle={"light-content"}
-                /> 
+  }, []);
 
-                <Cabecario abrirFecharMenu={abrirFecharMenu}/>
-                
-                { menuAtivo ? ( <Menu navegar={navigation}/> ) : "" }
-                
-                <Titulo titulo={"LOCALIZAÇÃO"}/>                
-                     <MapView style = {styles.map}
-                         showsUserLocation={true}		//destacando a localização do usuário no mapa
-                         showsMyLocationButton={false} 	//ocultando o botão que move o mapa para a localização do usuário
-                         toolbarEnabled={false}	//ocultando opções do google maps ao clicar em objetos do mapa
-                         onRegionChangeComplete={handleRegionChanged}  //mudar localização se estiver em movimento
-                         initialRegion={{
-                            latitude,	//posição inicial do mapa
-                            longitude,	//posição inicial do mapa
-                            latitudeDelta: 0.003,  	//determina o zoom do mapa
-                            longitudeDelta: 0.003
-                         }} />            
-             </LinearGradient>
-        
-        </View>
-        
-    );
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(markerAnimation, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(markerAnimation, {
+          toValue: 0,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  }, []);
 
-
+  return (
+    <View style={style.viewSize}>
+      <LinearGradient style={style.container} colors={['#063E56','#FFFFFF']}>
+        <StatusBar backgroundColor="#063E56" barStyle="light-content" />
+        <Cabecario abrirFecharMenu={abrirFecharMenu} />
+        { menuAtivo ? <Menu navegar={navigation} /> : "" }
+        <Titulo titulo={"LOCALIZAÇÃO"} />
+        <MapView
+          style={styles.map}
+          showsUserLocation={true}
+          showsMyLocationButton={false}
+          toolbarEnabled={false}
+          onRegionChangeComplete={handleRegionChanged}
+          initialRegion={{
+            latitude,
+            longitude,
+            latitudeDelta: 0.003,
+            longitudeDelta: 0.003
+          }}
+        >
+          <Polyline
+            coordinates={markerCoords}
+            strokeColor="blue"
+            strokeWidth={3}
+          />
+          <Marker.Animated
+            coordinate={{ latitude, longitude }}
+            image={require('../../../../assets/balao2.png')} // Altere o caminho para a sua imagem
+            style={{
+              transform: [{ scale: markerAnimation }],
+            }}
+          />
+        </MapView>
+      </LinearGradient>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    map: {
-      width: '100%',
-      height: '50%',
-    }
-  });
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '50%',
+  },
+});
