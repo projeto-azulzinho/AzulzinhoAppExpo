7a86bf6d46357c106e5deaab3a0e780a4c0099e1
costaemily <costa.emily@outlook.com>
2023-06-25 22:25:26 -0300

    add: dependecies from package-lock and yarn.lock

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Alert, StyleSheet, Text, View, Button } from "react-native";
-import { NavigationContainer } from '@react-navigation/native'
-import Routes from "./src/views/routes"
-import { Provider } from "react-redux"
+import { NavigationContainer } from "@react-navigation/native";
+import Routes from "./src/routes/index";
+import { Provider } from "react-redux";
 import { store } from "./src/redux/store";
 
 export default function App() {
-
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Routes/>
+        <Routes />
       </NavigationContainer>
     </Provider>
   );
 }
-
--- src/views/utilits/validation.js
+++
@@ -1,6 +0,0 @@
-length == 0 || null || undefined 
-
-const validation = (param) => {
-    if(param == null || param == undefined || param.length == 0)
-        return "Campo não preenchido"
-}
