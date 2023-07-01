899728451a9334b86a52f30fc602927ce5133d6c
Marina Giacchero <80406895+MarinaGiacchero@users.noreply.github.com>
2023-07-01 00:33:55 -0300

    Funcionando 1.0
    Está funcionando trazer os dados conforme o conjunto.

--- src/components/Tabela/index.js
+++ src/components/Tabela/index.js
@@ -1,82 +1,69 @@
-import {React, useState} from "react";
-import { View, Text } from "react-native"
-import style from "./style"
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import style from "./style";
 import { FlatList } from "react-native-gesture-handler";
 import { getSensorDeConjunto } from "../../controllers/SensorConjuntoController";
 import { getSensor } from "../../controllers/SensorController";
 
-export const Tabela = props => { 
-    const [result, setResult] = useState([])
-    const {resultado, conjunto} = props;
-    console.log(resultado)
-    
-    if(resultado.length != 0){
-    let resultado_final = resultado.split('|')
-
-    let ress = []
-    const filtroDeSensores =  () => {
-        
-         getSensorDeConjunto(conjunto.id).then(sensores => {
-            sensores.forEach( sensorId => {        
-                getSensor(sensorId).then(sensor => {
-                    index = resultado_final.indexOf(sensor.nomeSensor.stringValue)
-                    if(index !== -1){
-                        ress.push(resultado_final.splice(index, 2))
-                        if(result.length < 3)
-                            setResult(...result, ress)
-                       
-                    }
-                })
-        
-            })
-
-            
-       })
-    }
-
-    filtroDeSensores()
-
-    
-    
-    // console.log(result)
-    
-    
-    
+export const Tabela = (props) => {
+    const [result, setResult] = useState([]);
+    const { resultado, conjunto } = props;
+  
+    useEffect(() => {
+      if (resultado.length !== 0) {
+        const resultado_final = resultado.split("|");
+        const ress = [];
+  
+        const filtroDeSensores = () => {
+          getSensorDeConjunto(conjunto.id).then((sensores) => {
+            const promises = sensores.map((sensorId) => {
+              return getSensor(sensorId).then((sensor) => {
+                const nomeSensor = sensor.nomeSensor.stringValue;
+                const index = resultado_final.indexOf(nomeSensor);
+                if (index !== -1) {
+                  const valor = resultado_final[index + 1];
+                  ress.push({ nome: nomeSensor, valor: resultado_final[index + 1] });
+                }
+              });
+            });
+  
+            Promise.all(promises).then(() => {
+              if (ress.length > 0) {
+                setResult(ress);
+              }
+            });
+          });
+        };
+        if (conjunto) {
+          filtroDeSensores();
+        }
+      }
+    }, [resultado, conjunto]);
+  
     const dado = {
-        valores: result
-        // valores: [{"nome": "Temperatura", "valor": "28", "grandeza": "°C"},
-        //             {"nome": "Umidade", "valor": "75", "grandeza": "%"},
-        //             {"nome": "Pressao", "valor": "50", "grandeza": "Bar"}]
-    }
-    const info = [dado]
+      valores: result,
+    };
+  
+    const info = [dado];
+  
     return (
-        <FlatList
-            data={info}
-            renderItem={({item, index}) => {
-                return <View style={style.conjuntoDados}>{
-                    item.valores.map((valor, index) => {
-                        // console.log(valor)
-                            return (
-                                <View style={style.dado}>
-                                    <Text style={style.value}>{valor[0]}</Text>
-                                    <Text style={style.value}>{valor[1]}</Text>
-                                    {/* <Text style={style.value}>{valor.grandeza}</Text> */}
-                                </View>
-                            )
-                    })
-                }</View>
-            }}
-            keyExtractor={item => item}
-            horizontal={true}
-            style={style.list}
-            contentContainerStyle={style.contentLista}
-        />
-    )}else{
-        return(
-            <FlatList />
-
-        )
-
-
-    }
-}
+      <FlatList
+        data={info}
+        renderItem={({ item }) => (
+          <View style={style.conjuntoDados}>
+            {item.valores.map((valor, index) => (
+              <View style={style.dado} key={index}>
+                <Text style={style.value}>{valor.nome}</Text>
+                <Text style={style.value}>{valor.valor}</Text>
+                {/* <Text style={style.value}>{valor.grandeza}</Text> */}
+              </View>
+            ))}
+          </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        horizontal={true}
+        style={style.list}
+        contentContainerStyle={style.contentLista}
+      />
+    );
+  };
